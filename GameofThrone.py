b5bf3fdc2dda0dda867c4f4844de9b6eeca73982
damosm <damosm@free.fr>
2020-03-20 11:33:27 +0100

    yiyi

--- GameofThrone.py
+++ GameofThrone.py
@@ -3,7 +3,6 @@ import json
 import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
-import re
 import unicodedata
 import os
 
@@ -13,27 +12,7 @@ import os
 ########################################################################################################
 #chargement des fichiers
 ########################################################################################################""
-# with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt_Nettoyé\season1.json','r',encoding='utf-8') as f:
-#  data1 = json.load(f)
 
-# with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt_Nettoyé\season2.json','r',encoding='utf-8') as f:
-#  data2 = json.load(f)
-
-# with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt_Nettoyé\season3.json','r',encoding='utf-8') as f:
-#  data3 = json.load(f)
-
-# with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt_Nettoyé\season4.json','r',encoding='utf-8') as f:
-#  data4 = json.load(f)
-
-# with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt_Nettoyé\season5.json','r',encoding='utf-8') as f:
-#  data5 = json.load(f)
-
-# with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt_Nettoyé\season6.json','r',encoding='utf-8') as f:
-#  data6 = json.load(f)
- 
-# with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt_Nettoyé\season7.json','r',encoding='utf-8') as f:
-#  data7 = json.load(f)
-#############################################################################################
 with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-srt\season1.json','r',encoding='utf-8') as f:
  data1 = json.load(f)
 
@@ -58,20 +37,6 @@ with open(r'C:\Users\utilisateur\Documents\Python\GameofThrone\game-of-thrones-s
 liste_data = (data1,data2,data3,data4,data5,data6,data7)
 
 
-
-######################################################################""
-#essaie de nettoyage
-#####################################################################
-
-
-# for a , b in data.items() :
-#     # print("\n")
-#     # print(a, "\n")
-  
-#     for i,j in data[("{}").format(a)].items() :
-#       j = re.sub('"','',j.rstrip())
-#       # print(j)
-
 ##############################################################################################
 #create table
 ##############################################################################################
@@ -85,7 +50,7 @@ liste_data = (data1,data2,data3,data4,data5,data6,data7)
 
 #   cursor = cnx.cursor()
 
-#   cursor.execute('CREATE TABLE `dataia_nancy`.`gameofthrone` (`id` INT NOT NULL AUTO_INCREMENT,`second` VARCHAR(100) NULL,`text` VARCHAR(200) NULL,`saisonEpisode` VARCHAR(100) NULL,PRIMARY KEY (`id`));')
+#   cursor.execute('CREATE TABLE `dataia_nancy`.`gameofthrone` (`id` INT NOT NULL AUTO_INCREMENT,`saison` VARCHAR(100) NULL,`episode` VARCHAR(100) NULL,`titre` VARCHAR(100) NULL,`second` VARCHAR(100) NULL,`text` VARCHAR(200) NULL, PRIMARY KEY (`id`));')
   
 #   cnx.commit()
     
@@ -99,8 +64,6 @@ liste_data = (data1,data2,data3,data4,data5,data6,data7)
 #     if (cnx.is_connected()):
 #         cnx.close()
 #         print("MySQL connection is closed")
-
-
 
 
 
@@ -125,12 +88,16 @@ liste_data = (data1,data2,data3,data4,data5,data6,data7)
 #       print("\n")
 #       print(a, "\n")
     
-#       for i,j in data[("{}").format(a)].items() :
-
-#         # j = unicodedata.normalize('NFKD', j).encode('ascii', 'replace') #Nettoyage
-#         requete = ('insert into gameofthrone (second,text,saisonEpisode) values ("{}","{}","{}");').format(i, j, a)
-#         print(requete)
-#         cursor.execute(requete)
+#       for second,text in data[("{}").format(a)].items() :
+        
+#         saison = a[17:19]
+#         episode = a[20:22]
+#         titre = a[23:-4]
+#         text = unicodedata.normalize('NFKD', text).encode('ascii', 'replace') #Nettoyage
+#         requete = ('insert into gameofthrone (saison,episode,titre,second,text) values (%s,%s,%s,%s,%s);')        
+#         values = (saison,episode,titre,second,text)
+#         print(requete,values)
+#         cursor.execute(requete,values)
 
   
   
@@ -148,78 +115,34 @@ liste_data = (data1,data2,data3,data4,data5,data6,data7)
 #         cnx.close()
 #         print("MySQL connection is closed")
 
-################################################################################################
-#insert2
-################################################################################################
-
+##################################################################################################
+#select
+###################################################################################################
 try :
   
   cnx = mysql.connector.connect(user='root', password='damos02',
-                             host='127.0.0.1',
-                             database='dataia_nancy')
+                            host='127.0.0.1',
+                            database='dataia_nancy')
   if (cnx.is_connected()):
     print('is connected')
-    
+
   cursor = cnx.cursor()
-  
-  for data in liste_data :
-    
-    for a , b in data.items() :
-      print("\n")
-      print(a, "\n")
-    
-      for i,j in data[("{}").format(a)].items() :
 
-        j = unicodedata.normalize('NFKD', j).encode('ascii', 'replace') #Nettoyage
-        requete = ('insert into gameofthrone (second,text,saisonEpisode) values (%s,%s,%s);')        
-        values = (i,j,a)
-        print(requete,values)
-        cursor.execute(requete,values)
+  cursor.execute('select * from gameofthrone limit 10;')
 
   
+  row = cursor.fetchall()
+  result = pd.DataFrame(row)
+
+  print(result)
   
-  cnx.commit()
-  cursor.close()
-  
-  
+  cursor.close()  
   cnx.close()
-
+  
 except mysql.connector.Error as error:
-    print("Failed to insert record into gameofthrone table {}".format(error))
+    print("Failed to select record into gameofthrone table {}".format(error))
 
 finally:
     if (cnx.is_connected()):
         cnx.close()
         print("MySQL connection is closed")
-
-##################################################################################################
-#select
-###################################################################################################
-# try :
-  
-#   cnx = mysql.connector.connect(user='root', password='damos02',
-#                             host='127.0.0.1',
-#                             database='dataia_nancy')
-#   if (cnx.is_connected()):
-#     print('is connected')
-
-#   cursor = cnx.cursor()
-
-#   cursor.execute('select * from gameofthrone limit 10;')
-
-  
-#   row = cursor.fetchall()
-#   result = pd.DataFrame(row)
-
-#   print(result)
-  
-#   cursor.close()  
-#   cnx.close()
-  
-# except mysql.connector.Error as error:
-#     print("Failed to select record into gameofthrone table {}".format(error))
-
-# finally:
-#     if (cnx.is_connected()):
-#         cnx.close()
-#         print("MySQL connection is closed")
